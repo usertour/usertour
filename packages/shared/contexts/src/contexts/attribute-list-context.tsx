86cdfc280c872e55e1d140f174ca03db8ffad088
winter100004 <liuzhaodong@gmail.com>
2025-02-13 15:32:44 +0800

    feat(packages):  Adapt to Biome and Husky syntax checks

--- packages/shared/contexts/src/contexts/attribute-list-context.tsx
+++ packages/shared/contexts/src/contexts/attribute-list-context.tsx
@@ -1,7 +1,7 @@
-import { ReactNode, createContext, useContext } from "react";
-import { useQuery } from "@apollo/client";
-import { listAttributes } from "@usertour-ui/gql";
-import { Attribute } from "@usertour-ui/types";
+import { useQuery } from '@apollo/client';
+import { listAttributes } from '@usertour-ui/gql';
+import { Attribute } from '@usertour-ui/types';
+import { ReactNode, createContext, useContext } from 'react';
 
 export interface AttributeListProviderProps {
   children?: ReactNode;
@@ -12,37 +12,27 @@ export interface AttributeListContextValue {
   attributeList: Attribute[] | undefined;
   refetch: any;
 }
-export const AttributeListContext = createContext<
-  AttributeListContextValue | undefined
->(undefined);
+export const AttributeListContext = createContext<AttributeListContextValue | undefined>(undefined);
 
-export function AttributeListProvider(
-  props: AttributeListProviderProps
-): JSX.Element {
+export function AttributeListProvider(props: AttributeListProviderProps): JSX.Element {
   const { children, projectId } = props;
   const { data, refetch } = useQuery(listAttributes, {
     variables: { projectId: projectId, bizType: 0 },
   });
 
-  const attributeList = data && data.listAttributes;
+  const attributeList = data?.listAttributes;
   const value: AttributeListContextValue = {
     attributeList,
     refetch,
   };
 
-  return (
-    <AttributeListContext.Provider value={value}>
-      {children}
-    </AttributeListContext.Provider>
-  );
+  return <AttributeListContext.Provider value={value}>{children}</AttributeListContext.Provider>;
 }
 
 export function useAttributeListContext(): AttributeListContextValue {
   const context = useContext(AttributeListContext);
   if (!context) {
-    throw new Error(
-      `useAttributeListContext must be used within a AttributeListProvider.`
-    );
+    throw new Error('useAttributeListContext must be used within a AttributeListProvider.');
   }
   return context;
 }
--- packages/shared/contexts/src/contexts/theme-list-context.tsx
+++ packages/shared/contexts/src/contexts/theme-list-context.tsx
@@ -1,7 +1,7 @@
-import { ReactNode, createContext, useContext } from "react";
-import { Theme } from "@usertour-ui/types";
-import { useQuery } from "@apollo/client";
-import { listThemes } from "@usertour-ui/gql";
+import { useQuery } from '@apollo/client';
+import { listThemes } from '@usertour-ui/gql';
+import { Theme } from '@usertour-ui/types';
+import { ReactNode, createContext, useContext } from 'react';
 
 export interface ThemeListProviderProps {
   children?: ReactNode;
@@ -12,9 +12,7 @@ export interface ThemeListContextValue {
   themeList: Theme[] | null;
   refetch: any;
 }
-export const ThemeListContext = createContext<
-  ThemeListContextValue | undefined
->(undefined);
+export const ThemeListContext = createContext<ThemeListContextValue | undefined>(undefined);
 
 export function ThemeListProvider(props: ThemeListProviderProps): JSX.Element {
   const { children, projectId } = props;
@@ -22,25 +20,19 @@ export function ThemeListProvider(props: ThemeListProviderProps): JSX.Element {
     variables: { projectId: projectId },
   });
 
-  const themeList = data && data.listThemes;
+  const themeList = data?.listThemes;
   const value: ThemeListContextValue = {
     themeList,
     refetch,
   };
 
-  return (
-    <ThemeListContext.Provider value={value}>
-      {children}
-    </ThemeListContext.Provider>
-  );
+  return <ThemeListContext.Provider value={value}>{children}</ThemeListContext.Provider>;
 }
 
 export function useThemeListContext(): ThemeListContextValue {
   const context = useContext(ThemeListContext);
   if (!context) {
-    throw new Error(
-      `useThemeListContext must be used within a ThemeListProvider.`
-    );
+    throw new Error('useThemeListContext must be used within a ThemeListProvider.');
   }
   return context;
 }
