e168aaeadbb8ed2f1530142312bfe2740c1ef157
winter100004 <liuzhaodong@gmail.com>
2025-03-10 21:10:34 +0800

    refactor: Simplify registration page component structure

--- apps/web/src/pages/authentication/registration.tsx
+++ apps/web/src/pages/authentication/registration.tsx
@@ -7,32 +7,24 @@ import {
   RegistrationSubmitButton,
 } from './components/registration-form';
 
-const RegistrationBody = () => {
-  return (
-    <RegistrationRoot>
-      <CardContent className="grid gap-4">
-        <RegistrationFormFields />
-        <RegistrationSubmitButton />
-      </CardContent>
-    </RegistrationRoot>
-  );
-};
-
-RegistrationBody.displayName = 'RegistrationBody';
-
 const Registration = () => {
   return (
-    <Card>
-      <CardHeader className="space-y-1 text-center">
-        <CardTitle className="text-2xl font-semibold tracking-tight">
-          Set up your Usertour account!
-        </CardTitle>
-        <CardDescription className="text-sm text-muted-foreground">
-          Enter your email and password below to login your account
-        </CardDescription>
-        <RegistrationBody />
-      </CardHeader>
-    </Card>
+    <RegistrationRoot>
+      <Card>
+        <CardHeader className="space-y-1 text-center">
+          <CardTitle className="text-2xl font-semibold tracking-tight">
+            Set up your Usertour account!
+          </CardTitle>
+          <CardDescription className="text-sm text-muted-foreground">
+            Enter your email and password below to login your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <RegistrationFormFields />
+          <RegistrationSubmitButton />
+        </CardContent>
+      </Card>
+    </RegistrationRoot>
   );
 };
 
