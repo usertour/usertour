d194d4a6b548156f0c7ad646f96c3018d88a7246
winter100004 <liuzhaodong@gmail.com>
2025-03-18 16:44:51 +0800

    feat(editor): add error handling for empty question name in ContentEditorNPS component

--- packages/shared/editor/src/editor/components/nps.tsx
+++ packages/shared/editor/src/editor/components/nps.tsx
@@ -3,11 +3,13 @@ import { Input } from '@usertour-ui/input';
 import { Label } from '@usertour-ui/label';
 import { QuestionTooltip } from '@usertour-ui/tooltip';
 import { RulesCondition } from '@usertour-ui/types';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { ContentActions } from '../..';
 import { useContentEditorContext } from '../../contexts/content-editor-context';
 import { ContentEditorNPSElement } from '../../types/editor';
 import { Button } from '@usertour-ui/button';
+import { EditorError, EditorErrorContent } from '../../components/editor-error';
+import { EditorErrorAnchor } from '../../components/editor-error';
 
 const buttonBaseClass =
   'flex items-center overflow-hidden group font-semibold relative border border-sdk-question hover:bg-sdk-question/30  rounded-md main-transition p-2 py-2 text-base justify-center';
@@ -29,6 +31,7 @@ export const ContentEditorNPS = (props: ContentEditorNPSProps) => {
     createStep,
     attributes,
   } = useContentEditorContext();
+  const [isShowError, setIsShowError] = useState<boolean>(false);
   const [isOpen, setIsOpen] = useState<boolean>();
 
   // Handle question text change
@@ -53,79 +56,94 @@ export const ContentEditorNPS = (props: ContentEditorNPSProps) => {
     updateElement({ ...element, data: { ...element.data, [type]: value } }, id);
   };
 
-  return (
-    <Popover.Root modal={true} onOpenChange={setIsOpen} open={isOpen}>
-      <Popover.Trigger asChild>
-        <div className="cursor-pointer">
-          <div
-            className="grid gap-1.5 !gap-1"
-            style={{ gridTemplateColumns: 'repeat(11, minmax(0px, 1fr))' }}
-          >
-            {Array.from({ length: 11 }, (_, i) => (
-              <Button key={i} className={`${buttonBaseClass}`}>
-                {i}
-              </Button>
-            ))}
-          </div>
-          <div className="flex mt-2.5 px-0.5 text-[13px] items-center justify-between opacity-80">
-            <p>{element.data.lowLabel || 'Not at all likely'}</p>
-            <p>{element.data.highLabel || 'Extremely likely'}</p>
-          </div>
-        </div>
-      </Popover.Trigger>
+  useEffect(() => {
+    if (isOpen === false && element.data.name === '') {
+      setIsShowError(true);
+    } else {
+      setIsShowError(false);
+    }
+  }, [isOpen, element?.data?.name]);
 
-      <Popover.Portal>
-        <Popover.Content
-          className="z-50 w-72 rounded-md border bg-background p-4"
-          style={{ zIndex }}
-        >
-          <div className="flex flex-col gap-2.5">
-            <Label htmlFor="nps-question">Question name</Label>
-            <Input
-              id="nps-question"
-              value={element.data.name}
-              onChange={handleNameChange}
-              placeholder="Question name?"
-            />
-            <Label>When answer is submitted</Label>
-            <ContentActions
-              zIndex={zIndex}
-              isShowIf={false}
-              isShowLogic={false}
-              currentStep={currentStep}
-              currentVersion={currentVersion}
-              onDataChange={handleActionChange}
-              defaultConditions={element?.data?.actions || []}
-              attributes={attributes}
-              // segments={segmentList || []}
-              contents={contentList}
-              createStep={createStep}
-            />
-            <Label className="flex items-center gap-1">
-              Labels
-              <QuestionTooltip>
-                Below each option, provide labels to clearly convey their meaning, such as "Bad"
-                positioned under the left option and "Good" under the right.
-              </QuestionTooltip>
-            </Label>
-            <div className="flex flex-row gap-2">
-              <Input
-                type="text"
-                value={element.data.lowLabel}
-                placeholder="Default"
-                onChange={(e) => handleLabelChange(e.target.value, 'lowLabel')}
-              />
-              <Input
-                type="text"
-                value={element.data.highLabel}
-                placeholder="Default"
-                onChange={(e) => handleLabelChange(e.target.value, 'highLabel')}
-              />
+  return (
+    <EditorError open={isShowError}>
+      <EditorErrorAnchor>
+        <Popover.Root modal={true} onOpenChange={setIsOpen} open={isOpen}>
+          <Popover.Trigger asChild>
+            <div className="cursor-pointer">
+              <div
+                className="grid gap-1.5 !gap-1"
+                style={{ gridTemplateColumns: 'repeat(11, minmax(0px, 1fr))' }}
+              >
+                {Array.from({ length: 11 }, (_, i) => (
+                  <Button key={i} className={`${buttonBaseClass}`}>
+                    {i}
+                  </Button>
+                ))}
+              </div>
+              <div className="flex mt-2.5 px-0.5 text-[13px] items-center justify-between opacity-80">
+                <p>{element.data.lowLabel || 'Not at all likely'}</p>
+                <p>{element.data.highLabel || 'Extremely likely'}</p>
+              </div>
             </div>
-          </div>
-        </Popover.Content>
-      </Popover.Portal>
-    </Popover.Root>
+          </Popover.Trigger>
+
+          <Popover.Portal>
+            <Popover.Content
+              className="z-50 w-72 rounded-md border bg-background p-4"
+              style={{ zIndex }}
+            >
+              <div className="flex flex-col gap-2.5">
+                <Label htmlFor="nps-question">Question name</Label>
+                <Input
+                  id="nps-question"
+                  value={element.data.name}
+                  onChange={handleNameChange}
+                  placeholder="Question name?"
+                />
+                <Label>When answer is submitted</Label>
+                <ContentActions
+                  zIndex={zIndex}
+                  isShowIf={false}
+                  isShowLogic={false}
+                  currentStep={currentStep}
+                  currentVersion={currentVersion}
+                  onDataChange={handleActionChange}
+                  defaultConditions={element?.data?.actions || []}
+                  attributes={attributes}
+                  // segments={segmentList || []}
+                  contents={contentList}
+                  createStep={createStep}
+                />
+                <Label className="flex items-center gap-1">
+                  Labels
+                  <QuestionTooltip>
+                    Below each option, provide labels to clearly convey their meaning, such as "Bad"
+                    positioned under the left option and "Good" under the right.
+                  </QuestionTooltip>
+                </Label>
+                <div className="flex flex-row gap-2">
+                  <Input
+                    type="text"
+                    value={element.data.lowLabel}
+                    placeholder="Default"
+                    onChange={(e) => handleLabelChange(e.target.value, 'lowLabel')}
+                  />
+                  <Input
+                    type="text"
+                    value={element.data.highLabel}
+                    placeholder="Default"
+                    onChange={(e) => handleLabelChange(e.target.value, 'highLabel')}
+                  />
+                </div>
+              </div>
+            </Popover.Content>
+          </Popover.Portal>
+        </Popover.Root>
+      </EditorErrorAnchor>
+      <EditorErrorContent side="bottom" style={{ zIndex: zIndex }}>
+        Question name is required
+      </EditorErrorContent>
+    </EditorError>
   );
 };
 
