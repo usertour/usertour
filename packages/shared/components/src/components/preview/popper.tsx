694b087c8edbac65c83fd2063c436b0c7842421a
winter100004 <liuzhaodong@gmail.com>
2025-03-19 17:40:11 +0800

    refactor(sidebar): update content list structure and improve tooltip/modal handling in SidebarCreate component

--- packages/shared/components/src/components/preview/popper.tsx
+++ packages/shared/components/src/components/preview/popper.tsx
@@ -1,3 +1,4 @@
+import { EyeNoneIcon } from '@usertour-ui/icons';
 import * as SharedPopper from '@usertour-ui/sdk';
 import { ContentEditorSerialize } from '@usertour-ui/shared-editor';
 import { convertSettings, convertToCssVars } from '@usertour-ui/shared-utils';
@@ -32,29 +33,32 @@ export const PopperPreview = (props: CreatePopperContentProps) => {
         className="flex-none justify-center flex flex-col items-center h-44	"
         // style={{ transform: `scale(${scale})` }}
       >
-        <SharedPopper.Popper
-          triggerRef={ref}
-          open={true}
-          zIndex={1111}
-          globalStyle={convertToCssVars(convertSettings(defaultSettings))}
-        >
-          <SharedPopper.PopperStaticContent
-            arrowSize={{
-              width: 20,
-              height: 10,
-            }}
-            customStyle={{ zoom: scale }}
-            side="bottom"
-            showArrow={type === 'tooltip'}
-            width={width}
-            height={height}
-            arrowColor={defaultSettings?.mainColor.background}
+        {type !== 'hidden' && (
+          <SharedPopper.Popper
+            triggerRef={ref}
+            open={true}
+            zIndex={1111}
+            globalStyle={convertToCssVars(convertSettings(defaultSettings))}
           >
-            <SharedPopper.PopperClose />
-            {/* {...serialize} */}
-            <ContentEditorSerialize contents={data} />
-          </SharedPopper.PopperStaticContent>
-        </SharedPopper.Popper>
+            <SharedPopper.PopperStaticContent
+              arrowSize={{
+                width: 20,
+                height: 10,
+              }}
+              customStyle={{ zoom: scale }}
+              side="bottom"
+              showArrow={type === 'tooltip'}
+              width={width}
+              height={height}
+              arrowColor={defaultSettings?.mainColor.background}
+            >
+              <SharedPopper.PopperClose />
+              {/* {...serialize} */}
+              <ContentEditorSerialize contents={data} />
+            </SharedPopper.PopperStaticContent>
+          </SharedPopper.Popper>
+        )}
+        {type === 'hidden' && <EyeNoneIcon className="w-6 h-6" />}
       </div>
       <div className="bg-background-400 flex-none leading-8 text-foreground text-center rounded-b-lg">
         {text}
