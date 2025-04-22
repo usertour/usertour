a712181e2244b072ff4d0de0f3508b3ccb5f4800
Dev Dalia <devdalia9@gmail.com>
2025-04-22 23:06:34 +0530

    chore: log removed

--- apps/sdk/src/core/third-party-analytics/adapters/ga.ts
+++ apps/sdk/src/core/third-party-analytics/adapters/ga.ts
@@ -3,7 +3,6 @@ import { BaseAnalyticsAdapter } from './base-analytics-adapter';
 export class GoogleAnalyticsAdapter extends BaseAnalyticsAdapter {
   sendEvent(event: { eventName: string; userId?: string; eventData?: Record<string, any> }) {
     if (typeof this.w.gtag === 'function') {
-      console.log('event...', event, this.w.gtag);
       this.w.gtag('event', event.eventName, {
         user_id: event.userId,
         session_id: new Date().toISOString(),
