a5dbda49f1dfdd96509a622f4990660e07d4d72a
winter100004 <liuzhaodong@gmail.com>
2025-05-27 18:26:23 +0800

    fix: update Popper component styles and link for Usertour
    - Changed overflow style from 'scroll' to 'hidden' in PopperStaticContent for better layout control.
    - Updated the Usertour link to include a query parameter for tracking purposes, enhancing analytics capabilities.

--- packages/shared/sdk/src/popper.tsx
+++ packages/shared/sdk/src/popper.tsx
@@ -648,7 +648,7 @@ const PopperStaticContent = forwardRef<HTMLDivElement, PopperStaticContentProps>
           style={{
             width,
             height,
-            overflow: 'scroll',
+            overflow: 'hidden',
           }}
         >
           <div className="usertour-widget-popper__frame-wrapper">
@@ -699,11 +699,10 @@ const PopperMadeWith = forwardRef<HTMLDivElement>((_, ref) => {
     <>
       <div ref={ref} className="absolute bottom-2 left-3 text-xs	opacity-50	hover:opacity-75		">
         <a
-          href="https://www.usertour.io"
+          href="https://www.usertour.io?utm_source=usertour-widget"
           className="!text-sdk-foreground !no-underline	 flex flex-row space-x-0.5 items-center !font-sans "
           target="_blank"
           rel="noreferrer"
-          // onClick={()=>{window.top?.open("https://www.usertour.io", "_blank")}}
         >
           <UsertourIcon width={14} height={14} />
           <span>Made with Usertour</span>
