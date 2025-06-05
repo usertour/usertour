70c677a2c101c24a4b21505b46fc6aae9659a2a2
winter100004 <liuzhaodong@gmail.com>
2025-06-05 09:39:35 +0800

    chore: comment out unused bizSession update logic in seed script
    - Commented out the logic for updating bizSession and bizAnswer with environmentId in the seed script for clarity and to prevent execution during seeding.
    - This change maintains the structure of the script while ensuring that the commented code remains accessible for future reference.

--- apps/server/prisma/seed.ts
+++ apps/server/prisma/seed.ts
@@ -4,6 +4,24 @@ const prisma = new PrismaClient();
 
 async function main() {
   const user = await prisma.user.findFirst();
+  // const bizSessions = await prisma.bizSession.findMany({
+  //   where: { environmentId: null },
+  //   include: { content: true },
+  // });
+  // for (const bizSession of bizSessions) {
+  //   await prisma.bizSession.update({
+  //     where: { id: bizSession.id },
+  //     data: {
+  //       environmentId: bizSession.content.environmentId,
+  //     },
+  //   });
+  //   await prisma.bizAnswer.updateMany({
+  //     where: { bizSessionId: bizSession.id },
+  //     data: {
+  //       environmentId: bizSession.content.environmentId,
+  //     },
+  //   });
+  // }
   if (user) {
     console.log('User already exists...');
     return;
