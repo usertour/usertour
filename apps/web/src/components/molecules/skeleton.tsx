2ec5edd87ec2af21e641afdf8e83ed09ca47fb37
winter100004 <liuzhaodong@gmail.com>
2025-06-29 09:40:24 +0800

    feat: update API endpoints and enhance ListSkeleton component
    - Changed API proxy targets from localhost to production URLs for /graphql and /api.
    - Refactored ListSkeleton component to utilize a table structure for improved loading state representation.
    - Added multiple skeleton elements within a table layout to enhance user experience during data loading.

--- apps/web/src/components/molecules/skeleton.tsx
+++ apps/web/src/components/molecules/skeleton.tsx
@@ -1,14 +1,51 @@
 import { Skeleton } from '@usertour-ui/skeleton';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@usertour-ui/table';
 
 export const ListSkeleton = () => {
   return (
-    <div className="flex flex-col space-y-2 ">
-      <Skeleton className="h-6 w-full" />
-      <Skeleton className="h-6 w-10/12" />
-      <Skeleton className="h-6 w-8/12" />
-      <Skeleton className="h-6 w-5/12" />
-      <Skeleton className="h-6 w-11/12" />
-      <Skeleton className="h-6 w-full" />
+    <div className="rounded-md border-none">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>
+              <Skeleton className="h-4 w-36" />
+            </TableHead>
+            <TableHead>
+              <div className="flex items-center space-x-1">
+                <Skeleton className="h-4 w-32" />
+                <Skeleton className="h-4 w-4 rounded" />
+              </div>
+            </TableHead>
+            <TableHead>
+              <Skeleton className="h-4 w-24" />
+            </TableHead>
+            <TableHead>
+              <Skeleton className="h-4 w-8" />
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {Array.from({ length: 5 }, (_, index) => (
+            <TableRow key={index}>
+              <TableCell>
+                <Skeleton className="h-4 w-32" />
+              </TableCell>
+              <TableCell>
+                <div className="flex items-center space-x-1">
+                  <Skeleton className="h-4 w-48" />
+                  <Skeleton className="h-6 w-6 rounded" />
+                </div>
+              </TableCell>
+              <TableCell>
+                <Skeleton className="h-4 w-36" />
+              </TableCell>
+              <TableCell>
+                <Skeleton className="h-8 w-8 rounded" />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 };
