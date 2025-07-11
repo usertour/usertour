aa828012c33e091cd2c58e3b2261023be20b0b23
winter100004 <liuzhaodong@gmail.com>
2025-07-11 14:14:56 +0800

    refactor(Popper): move transformOrigin middleware to position utils
    - Moved the transformOrigin middleware and getSideAndAlignFromPlacement function from the Popper component to the position utility file for better organization and reusability.
    - Updated imports in the Popper component to reflect the new structure, enhancing code clarity and maintainability.

--- packages/shared/sdk/src/popper.tsx
+++ packages/shared/sdk/src/popper.tsx
@@ -18,7 +18,11 @@ import {
 } from '@floating-ui/react-dom';
 import type { SideObject, Rect, Placement, Middleware } from '@floating-ui/dom';
 import { positionModal, getReClippingRect, getViewportRect } from './utils/backdrop';
-import { computePositionStyle } from './utils/position';
+import {
+  computePositionStyle,
+  getSideAndAlignFromPlacement,
+  transformOrigin,
+} from './utils/position';
 import { cn } from '@usertour-ui/ui-utils';
 import { Align, ProgressBarType, Side } from '@usertour-ui/types';
 import { hiddenStyle } from './utils/content';
@@ -574,51 +578,6 @@ const PopperClose = forwardRef<HTMLButtonElement, PopoverCloseProps>(
 );
 
 PopperClose.displayName = CLOSE_NAME;
-
-const transformOrigin = (options: {
-  arrowWidth: number;
-  arrowHeight: number;
-}): Middleware => ({
-  name: 'transformOrigin',
-  options,
-  fn(data) {
-    const { placement, rects, middlewareData } = data;
-
-    const cannotCenterArrow = middlewareData.arrow?.centerOffset !== 0;
-    const isArrowHidden = cannotCenterArrow;
-    const arrowWidth = isArrowHidden ? 0 : options.arrowWidth;
-    const arrowHeight = isArrowHidden ? 0 : options.arrowHeight;
-
-    const [placedSide, placedAlign] = getSideAndAlignFromPlacement(placement);
-    const noArrowAlign = { start: '0%', center: '50%', end: '100%' }[placedAlign];
-
-    const arrowXCenter = (middlewareData.arrow?.x ?? 0) + arrowWidth / 2;
-    const arrowYCenter = (middlewareData.arrow?.y ?? 0) + arrowHeight / 2;
-
-    let x = '';
-    let y = '';
-
-    if (placedSide === 'bottom') {
-      x = isArrowHidden ? noArrowAlign : `${arrowXCenter}px`;
-      y = `${-arrowHeight}px`;
-    } else if (placedSide === 'top') {
-      x = isArrowHidden ? noArrowAlign : `${arrowXCenter}px`;
-      y = `${rects.floating.height + arrowHeight}px`;
-    } else if (placedSide === 'right') {
-      x = `${-arrowHeight}px`;
-      y = isArrowHidden ? noArrowAlign : `${arrowYCenter}px`;
-    } else if (placedSide === 'left') {
-      x = `${rects.floating.width + arrowHeight}px`;
-      y = isArrowHidden ? noArrowAlign : `${arrowYCenter}px`;
-    }
-    return { data: { x, y } };
-  },
-});
-
-function getSideAndAlignFromPlacement(placement: Placement) {
-  const [side, align = 'center'] = placement.split('-');
-  return [side as Side, align as Align] as const;
-}
 
 type PopperStaticContentProps = PopperContentProps & {
   height: string;
