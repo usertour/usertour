0a600fdb8f82ee53b427730400c4edeb45161e5f
winter100004 <liuzhaodong@gmail.com>
2025-08-13 16:52:28 +0800

    refactor: simplify conditions comparison logic
    - Removed the compareConditionsItem function and streamlined the conditionsIsSame function to directly use isEqual for comparing conditions arrays.
    - This change enhances code readability and maintainability by reducing complexity in the conditions comparison logic.

--- packages/shared/helpers/src/conditions/condition.ts
+++ packages/shared/helpers/src/conditions/condition.ts
@@ -1,59 +1,8 @@
 import { RulesCondition, RulesType } from '@usertour/types';
 import isEqual from 'fast-deep-equal';
 
-const compareConditionsItem = (item1: RulesCondition, item2: RulesCondition) => {
-  const { data = {}, ...others1 } = item1;
-  const { data: data2 = {}, ...others2 } = item2;
-  if (!isEqual(others2, others1)) {
-    return false;
-  }
-  for (const key in data) {
-    if (!isEqual(data[key], data2[key])) {
-      return false;
-    }
-  }
-  return true;
-};
-
 const conditionsIsSame = (rr1: RulesCondition[], rr2: RulesCondition[]) => {
-  const r1 = [...rr1];
-  const r2 = [...rr2];
-  if (r1.length === 0 && r2.length === 0) {
-    return true;
-  }
-  if (r1.length !== r2.length) {
-    return false;
-  }
-  const group1 = r1.filter((item) => item.type === 'group');
-  const group2 = r2.filter((item) => item.type === 'group');
-  if (group1.length !== group2.length) {
-    return false;
-  }
-  for (let index = 0; index < r1.length; index++) {
-    const item1 = r1[index];
-    const item2 = r2[index];
-    if (!item1 || !item2) {
-      return false;
-    }
-    if (item1.type === 'group') {
-      if (!item2.conditions) {
-        return false;
-      }
-      const c1 = item1.conditions as RulesCondition[];
-      const c2 = item2.conditions as RulesCondition[];
-      if (item1.operators !== item2.operators) {
-        return false;
-      }
-      if (!conditionsIsSame(c1, c2)) {
-        return false;
-      }
-    } else {
-      if (!compareConditionsItem(item1, item2)) {
-        return false;
-      }
-    }
-  }
-  return true;
+  return isEqual(rr1, rr2);
 };
 
 const isConditionsActived = (conditions: RulesCondition[]): boolean => {
